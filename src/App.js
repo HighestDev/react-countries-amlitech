25083a19c463ae6ded8ff010a651a14e48198421
HighestDev <kenneth.danso@amalitech.org>
2022-07-23 16:48:49 +0100

    create-react-app and configure Sass for styling

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Styles/styles.css';
+
+
+function App() {
+  return (
+    <div className="app">
+      <h1>HighestDev</h1>
+    </div>
+  )
+}
+
+export default App
+
